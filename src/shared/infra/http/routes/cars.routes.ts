92dcab4484118bc38b7e4ebd1008ae70695e1084
Renatoogata <renatoogata95@gmail.com>
2022-07-26 22:40:06 -0300

    entidade de carros, middleware de adm e funcs

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,10 +1,19 @@
-import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 import { Router } from 'express';
+
+import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ListAvailableCarController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarController';
+
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController()
+const listAvailableCarsController = new ListAvailableCarController();
 
-carsRoutes.post("/", createCarController.handle);
+carsRoutes.post("/", ensureAuthenticated, ensureAdmin, createCarController.handle);
+
+carsRoutes.get("/available", listAvailableCarsController.handle);
 
 export { carsRoutes }
--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -4,6 +4,8 @@ import multer from 'multer';
 import { CreateCategoryController } from '@modules/cars/useCases/createCategory/CreateCategoryController';
 import { ImportCategoryController } from '@modules/cars/useCases/importCategory/ImportCategoryController';
 import { ListCategoriesController } from '@modules/cars/useCases/listCategories/ListCategoriesController';
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
 
 
 const categoriesRoutes = Router();
@@ -16,10 +18,10 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoryController = new ListCategoriesController();
 
-categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.post("/", ensureAuthenticated, ensureAdmin, createCategoryController.handle);
 
 categoriesRoutes.get("/", listCategoryController.handle);
 
-categoriesRoutes.post("/import", upload.single("file"), importCategoryController.handle);
+categoriesRoutes.post("/import", ensureAuthenticated, ensureAdmin, upload.single("file"), importCategoryController.handle);
 
 export { categoriesRoutes };
